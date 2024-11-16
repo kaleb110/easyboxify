da4b08af0c587c90c4235457864d1120e2cd060b
kaleb <kalisha123k@gmail.com>
2024-11-16 16:58:20 +0300

    sidebar fix new

--- frontend/components/sidebar/sidebar-items.tsx
+++ frontend/components/sidebar/sidebar-items.tsx
@@ -23,7 +23,7 @@ export function SidebarItems({ onItemClick }: { onItemClick: (action: string) =>
           All Bookmarks
         </Button>
         <FolderSection onItemClick={onItemClick} />
-        <TagSection />
+        <TagSection onItemClick={onItemClick} />
       </div>
     </div>
   )
