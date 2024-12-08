3a25eca989fd82a2fdc69a1d1e792da2e9bc9bef
kaleb <kalisha123k@gmail.com>
2024-12-08 22:13:00 +0300

    typo fix

--- backend/dist/routes/deleteDataRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const deleteDataController_1 = require("../controller/deleteDataController");
-const DeleteDatarouter = (0, express_1.Router)();
-DeleteDatarouter.delete("/", deleteDataController_1.deleteDataController);
-exports.default = DeleteDatarouter;
